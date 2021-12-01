e7ec5f3ca4dc21ac22e07e8330021a5dd3b294ea
whiter4bbit <wr4bbit@gmail.com>
2021-12-01 13:17:37 +0100

    day 1

---
+++ 2021/day1.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(define (count-incr depths)
+    (for/sum ([p depths]
+              [c (rest depths)])
+              (if (> c p) 1 0)))
+
+(define (part-1 path)
+    (define depths (map string->number (file->lines path)))
+    (count-incr depths))
+
+(printf "Part-1: ~a\n" (part-1 "day1-input.txt"))
+
+(define (part-2 path)
+    (define depths (map string->number (file->lines path)))
+    (define window-sum
+        (for/list ([a depths]
+                   [b (cdr depths)]
+                   [c (cddr depths)])
+                  (+ a b c)))
+    (count-incr window-sum))
+
+(printf "Part-2: ~a\n" (part-2 "day1-input.txt"))
