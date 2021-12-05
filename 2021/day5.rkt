b8e1c3285d7672bb65c8de7db19dc91ba7bc8115
whiter4bbit <wr4bbit@gmail.com>
2021-12-06 00:07:22 +0100

    Use mutable hash

--- 2021/day5.rkt
+++ 2021/day5.rkt
@@ -4,34 +4,27 @@
 (define (vents-line-straight? vt)
   (let ([deg (abs (radians->degrees (angle (vents-line-dir vt))))])
     (or (= deg 0) (= deg 90) (= deg 180))))
-(define (vents-line-straight-or-diagonal? vt)
-  (or (vents-line-straight? vt)
-      (= (abs (radians->degrees (angle (vents-line-dir vt)))) 45)))
 (define (vents-line-collect-points vt points)
-  (define (collect points cur)
-    (let ([next-points (hash-update points cur add1 0)])
-      (if (= cur (vents-line-end vt))
-          next-points
-          (collect next-points (+ cur (vents-line-dir vt))))))
-  (collect points (vents-line-start vt)))
+  (define (collect cur)
+    (hash-update! points cur add1 0)
+    (when (not (= cur (vents-line-end vt)))
+      (collect (+ cur (vents-line-dir vt)))))
+  (collect (vents-line-start vt)))
 
 (define (read-lines path)
   (for/list ([raw (file->lines path)])
     (let* ([line (map string->number (regexp-split #px"\\,| \\-\\> " raw))]
            [start (make-rectangular (first line) (second line))]
            [end (make-rectangular (third line) (fourth line))]
-           [diff (- end start)])
-      (vents-line
-       start
-       end
-       (/ diff (max (abs (real-part diff)) (abs (imag-part diff))))))))
+           [diff (- end start)]
+           [dir (/ diff (max (abs (real-part diff)) (abs (imag-part diff))))])
+      (vents-line start end dir))))
 
 (define (count-points lines type)
-  (for/fold ([points (hash)]
-             #:result (for/sum ([(pos c) points] #:when (> c 1)) 1))
-            ([line lines]
-             #:when (type line))
-    (vents-line-collect-points line points)))
+  (define points (make-hash))
+  (for ([line lines] #:when(type line))
+    (vents-line-collect-points line points))
+  (for/sum ([(pos c) points] #:when (> c 1)) 1))
 
 (printf "Part-1: ~a\n" (count-points (read-lines "day5-input.txt") vents-line-straight?))
-(printf "Part-2: ~a\n" (count-points (read-lines "day5-input.txt") vents-line-straight-or-diagonal?))
+(printf "Part-2: ~a\n" (count-points (read-lines "day5-input.txt") (lambda (x) #t)))
