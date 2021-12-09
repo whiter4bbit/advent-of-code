dfa26d2bf9f7be4fc7d802942a0951fe25be0398
whiter4bbit <wr4bbit@gmail.com>
2021-12-09 14:55:15 +0100

    Day 9

--- 2021/day9.rkt
+++ 2021/day9.rkt
@@ -2,28 +2,33 @@
 
 (define (read-heightmap path)
   (for/fold ([heightmap (hash)])
-            ([row (file->lines path)]
-             [i (in-naturals)])
+            ([(row i) (in-indexed (file->lines path))])
     (for/fold ([heightmap heightmap])
-              ([cell row]
-               [j (in-naturals)])
+              ([(cell j) (in-indexed row)])
       (hash-set heightmap (cons i j) (- (char->integer cell) (char->integer #\0))))))
 
-(define (neighbors pos)
-  (for/list ([offset '((-1 . 0) (1 . 0) (0 . 1) (0 . -1))])
-    (cons (+ (car pos) (car offset)) (+ (cdr pos) (cdr offset)))))
+(define (neighbors heightmap pos)
+  (for/fold ([neighbors empty])
+            ([offset '((-1 . 0) (1 . 0) (0 . 1) (0 . -1))])
+    (let ([next (cons (+ (car pos) (car offset)) (+ (cdr pos) (cdr offset)))])
+      (if (hash-has-key? heightmap next)
+          (cons next neighbors)
+          neighbors))))
 
-(define (low-positions heightmap)
-  (define (low-pos? pos)
-    (for/and ([nei (neighbors pos)])
-      (< (hash-ref heightmap pos #f) (hash-ref heightmap nei +inf.0))))
-  (for/list ([pos (in-hash-keys heightmap)]
-             #:when (low-pos? pos))
-    pos))
+(define (cell-ref heightmap pos)
+  (hash-ref heightmap pos #f))
+
+(define (in-positions heightmap)
+  (in-hash-keys heightmap))
+
+(define (low-position? heightmap pos)
+  (for/and ([nei-pos (neighbors heightmap pos)])
+    (< (cell-ref heightmap pos) (cell-ref heightmap nei-pos))))
 
 (define (part-1 heightmap)
-  (for/sum ([pos (low-positions heightmap)])
-    (add1 (hash-ref heightmap pos #f))))
+  (for/sum ([pos (in-positions heightmap)]
+            #:when (low-position? heightmap pos))
+    (add1 (cell-ref heightmap pos))))
 
 (printf "Part-1: ~a\n" (part-1 (read-heightmap "day9-input.txt")))
 
@@ -32,14 +37,15 @@
   (define (basin-size pos)
     (set-add! seen pos)
     (for/fold ([size 1])
-              ([nei (neighbors pos)]
+              ([nei (neighbors heightmap pos)]
                #:when (and (not (set-member? seen nei))
-                           (< (hash-ref heightmap nei +inf.0) 9)
-                           (< (hash-ref heightmap pos #f) (hash-ref heightmap nei -inf.0))))
+                           (< (cell-ref heightmap nei) 9)
+                           (< (cell-ref heightmap pos) (cell-ref heightmap nei))))
       (+ size (basin-size nei))))
   (for/fold ([sizes empty]
              #:result (apply * (take (sort sizes >) 3)))
-            ([pos (low-positions heightmap)])
+            ([pos (in-positions heightmap)]
+             #:when (low-position? heightmap pos))
     (cons (basin-size pos) sizes)))
 
 (printf "Part-2: ~a\n" (part-2 (read-heightmap "day9-input.txt")))
