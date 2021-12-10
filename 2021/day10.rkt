f6239edb79815efc7116f0dfadf7c7c3d8b6712d
whiter4bbit <wr4bbit@gmail.com>
2021-12-10 13:49:29 +0100

    Day 10

---
+++ 2021/day10.rkt
@@ -0,0 +1,48 @@
+#lang racket
+
+(define (read-chunks path)
+  (map string->list (file->lines path)))
+
+(define open-brackets (seteqv #\( #\[ #\{ #\<))
+(define closing-brackets #hasheqv((#\) . #\() (#\] . #\[) (#\} . #\{) (#\> . #\<)))
+(define error-costs #hash((#\) . 3) (#\] . 57) (#\} . 1197) (#\> . 25137)))
+(define completion-costs #hash((#\( . 1) (#\[ . 2) (#\{ . 3) (#\< . 4)))
+
+(define (open? ch)
+  (set-member? open-brackets ch))
+(define (can-close? cur top)
+  (eqv? (hash-ref closing-brackets cur #f) top))
+
+(define (match-chunk chunk)
+  (let do-match ([stack empty]
+                 [chunk chunk])
+    (cond
+      [(empty? chunk) (cons 'success stack)]
+      [(open? (car chunk)) (do-match (cons (car chunk) stack) (cdr chunk))]
+      [(can-close? (car chunk) (car stack)) (do-match (cdr stack) (cdr chunk))]
+      [else (cons 'failure (car chunk))])))
+
+(define match-results
+  (for/list ([chunk (read-chunks "day10-input.txt")])
+    (match-chunk chunk)))
+
+(define (part-1 match-results)
+  (for/sum ([result match-results]
+            #:when (eqv? 'failure (car result)))
+    (hash-ref error-costs (cdr result) #f)))
+
+(printf "Part-1: ~a\n" (part-1 match-results))
+
+(define (completion-score stack)
+  (for/fold ([score 0])
+            ([cur stack])
+    (+ (* score 5) (hash-ref completion-costs cur #f))))
+
+(define (part-2 match-results)
+  (define scores
+    (for/vector ([result match-results]
+                 #:when (eq? 'success (car result)))
+      (completion-score (cdr result))))
+  (vector-ref (vector-sort scores <) (quotient (vector-length scores) 2)))
+
+(printf "Part-2: ~a\n" (part-2 match-results))
