763c8b0335ca8d24cdf60afcc5bf4e0a677f9e53
whiter4bbit <wr4bbit@gmail.com>
2021-12-16 02:08:49 +0100

    Use dijkstra (again)

--- 2021/day15.rkt
+++ 2021/day15.rkt
@@ -1,56 +1,66 @@
 #lang racket
 
-(require racket/match)
+(require data/heap)
 
-(define (read-chitons path)
-  (for/fold ([map* (hash)])
+(struct grid (cells size) #:transparent)
+(struct cell (row col value) #:transparent)
+
+(define (read-grid path)
+  (for/fold ([cells (hash)]
+             #:result (grid cells (add1 (argmax identity (map car (hash-keys cells))))))
             ([(row i) (in-indexed (file->lines path))])
-    (for/fold ([map* map*])
-              ([(cell j) (in-indexed row)])
-      (hash-set map* (cons i j) (- (char->integer cell) (char->integer #\0))))))
+    (for/fold ([cells cells])
+              ([(value j) (in-indexed row)])
+      (hash-set cells (cons i j) (cell i j (- (char->integer value) (char->integer #\0)))))))
 
-(define chitons (read-chitons "day15-input.txt"))
-(define inf-cost 1000000000)
+(define (grid-start-cell gr)
+  (hash-ref (grid-cells gr) '(0 . 0) #f))
 
-(define (neighbors pos)
-  (for/list ([offset '((-1 . 0) (1 . 0) (0 . -1) (0 . 1))])
-    (cons (+ (car pos) (car offset)) (+ (cdr pos) (cdr offset)))))
+(define (grid-target-cell gr)
+  (hash-ref (grid-cells gr) (cons (sub1 (grid-size gr)) (sub1 (grid-size gr)))))
 
-(define (lowest-risk target-pos chiton-cost)
+(define (grid-neighbors gr cur)
+  (filter
+   identity
+   (map (λ (p) (hash-ref (grid-cells gr) p #f))
+        (list (cons (sub1 (cell-row cur)) (cell-col cur))
+              (cons (add1 (cell-row cur)) (cell-col cur))
+              (cons (cell-row cur) (sub1 (cell-col cur)))
+              (cons (cell-row cur) (add1 (cell-col cur)))))))
+
+(define (dijkstra gr)
+  (struct node (cell cost))
+  (define min-nodes (make-heap (λ (a b) (<= (node-cost a) (node-cost b)))))
   (define costs (make-hash))
-  (hash-set! costs '(-1 . 0) (- (chiton-cost '(0 . 0))))
-  (for* ([row (in-range (add1 (car target-pos)))]
-         [col (in-range (add1 (cdr target-pos)))])
-    (define left-cost (hash-ref! costs (cons row (sub1 col)) inf-cost))
-    (define top-cost (hash-ref! costs (cons (sub1 row) col) inf-cost))
-    (hash-set! costs (cons row col) (+ (min left-cost top-cost) (chiton-cost (cons row col)))))
-  (hash-ref! costs target-pos #f))
-
-(define (single-tile-bottom-corner chitons)
-  (cons (car (argmax car (hash-keys chitons)))
-        (cdr (argmax cdr (hash-keys chitons)))))
-
-(define (single-tile-cost chitons)
-  (lambda (pos) (hash-ref chitons pos inf-cost)))
-
-(printf "Part-1: ~a\n" (lowest-risk (single-tile-bottom-corner chitons) (single-tile-cost chitons)))
-
-(define (multi-tile-bottom-corner chitons)
-  (match-define (cons row col) (single-tile-bottom-corner chitons))
-  (cons (sub1 (* (add1 row) 5)) (sub1 (* (add1 col) 5))))
-
-(define (multi-tile-cost chitons)
-  (define single-tile-size (add1 (car (single-tile-bottom-corner chitons))))
-  (define multi-tile-size (* single-tile-size 5))
-  (lambda (pos)
-    (match-define (cons row col) pos)
+  (hash-set! costs (grid-start-cell gr) 0)
+  (heap-add! min-nodes (node (grid-start-cell gr) 0))
+  (let loop ([cur (heap-min min-nodes)])
+    (heap-remove-min! min-nodes)
     (cond
-      [(or (< row 0) (< col 0) (>= row multi-tile-size) (>= col multi-tile-size)) inf-cost]
+      [(not (equal? (node-cost cur) (hash-ref! costs (node-cell cur) #f)))
+       (loop (heap-min min-nodes))]
+      [(equal? (node-cell cur) (grid-target-cell gr))
+       (node-cost cur)]
       [else
-       (define row-factor (quotient row single-tile-size))
-       (define col-factor (quotient col single-tile-size))
-       (define cost* ((single-tile-cost chitons) (cons (remainder row single-tile-size)
-                                                       (remainder col single-tile-size))))
-       (add1 (remainder (sub1 (+ cost* (+ row-factor col-factor))) 9))])))
+       (for ([nei (grid-neighbors gr (node-cell cur))])
+         (define next (node nei (+ (cell-value nei) (node-cost cur))))
+         (when (> (hash-ref! costs nei +inf.0) (node-cost next))
+           (heap-add! min-nodes next)
+           (hash-set! costs nei (node-cost next))))
+       (loop (heap-min min-nodes))])))
 
-(printf "Part-2: ~a\n" (lowest-risk (multi-tile-bottom-corner chitons) (multi-tile-cost chitons)))
+(printf "Part-1: ~a\n" (dijkstra (read-grid "day15-input.txt")))
+
+(define (grid-expand gr)
+  (define expanded-size (* (grid-size gr) 5))
+  (for*/fold ([cells (hash)]
+              #:result (grid cells expanded-size))
+             ([(_ cur) (grid-cells gr)]
+              [row-f (in-range 0 expanded-size (grid-size gr))]
+              [col-f (in-range 0 expanded-size (grid-size gr))])
+    (define row* (+ (cell-row cur) row-f))
+    (define col* (+ (cell-col cur) col-f))
+    (define value* (add1 (remainder (sub1 (+ (cell-value cur) (+ row-f col-f))) 9)))
+    (hash-set cells (cons row* col*) (cell row* col* value*))))
+
+(printf "Part-2: ~a\n" (dijkstra (grid-expand (read-grid "day15-input.txt"))))
