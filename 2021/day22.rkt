24e1fc60a07790a40155dec79dacc79e41bf58e2
Pavel Zalunin <wr4bbit@gmail.com>
2021-12-23 01:56:40 +0100

    Day 22: Improve perf (minutes -> 10s)

--- 2021/day22.rkt
+++ 2021/day22.rkt
@@ -10,28 +10,6 @@
 (define (read-steps path)
   (map string->step (file->lines path)))
 
-(define (cube-within? cube dim-ranges)
-  (for/and ([dim cube]
-            [dim-range dim-ranges])
-    (and (>= dim (car dim-range)) (<= dim (cdr dim-range)))))
-
-(define (part-1 steps)
-  (define cubes
-    (for*/list ([x (in-range -50 51)]
-                [y (in-range -50 51)]
-                [z (in-range -50 51)])
-      (list x y z)))
-  (define states (make-hash))
-  (for* ([cube cubes]
-         [step steps]
-         #:when (cube-within? cube (cdr step)))
-    (hash-set! states cube (car step)))
-  (for/sum ([(cube state) (in-hash states)]
-            #:when (eq? state 'on))
-    1))
-
-(printf "Part-1: ~a\n" (part-1 (read-steps "day22-input.txt")))
-
 (define (axis-values steps p)
   (for/fold ([v (seteqv)]
              #:result
@@ -55,18 +33,22 @@
       [(< (vector-ref vt m) v) (find (+ m 1) hi)]
       [else (find lo (- m 1))])))
 
-(define (2d-vector-set vt r c v)
+(define (matrix-set vt r c v)
   (vector-set! (vector-ref vt r) c v))
 
-(define (count-on steps)
+(define (count-layer steps)
   (define y-axis (axis-values steps third))
   (define z-axis (axis-values steps fourth))
-  (define layer (build-vector (vector-length y-axis) (λ (_) (make-vector (vector-length z-axis) 0))))
+  (define layer
+    (build-vector
+     (vector-length y-axis)
+     (λ (_) (make-vector
+             (vector-length z-axis) 0))))
   (for ([step steps])
     (match-define (list st _ (cons y1 y2) (cons z1 z2)) step)
     (for* ([r (in-range (vector-ceil y-axis y1) (add1 (vector-ceil y-axis y2)))]
            [c (in-range (vector-ceil z-axis z1) (add1 (vector-ceil z-axis z2)))])
-      (2d-vector-set layer r c (if (eq? st 'on) 1 0))))
+      (matrix-set layer r c (if (eq? st 'on) 1 0))))
   (for/sum ([(row r) (in-indexed layer)])
     (for/sum ([(cell c) (in-indexed row)]
               #:when (= cell 1))
@@ -74,17 +56,35 @@
       (define w (- (vector-ref z-axis c) (vector-ref z-axis (sub1 c))))
       (* h w))))
 
-(define (part-2 steps)
+(define (count-on steps)
   (define x-axis (vector->list (axis-values steps second)))
   (define x-min (argmin identity x-axis))
   (define x-max (argmax identity x-axis))
+  (define seen (make-hash))
+  (define (count-layer/memo steps)
+    (cond
+      [(hash-has-key? seen steps) (hash-ref! seen steps #f)]
+      [else
+       (define res (count-layer steps))
+       (hash-set! seen steps res)
+       res]))
   (for/sum ([x (in-range x-min (add1 x-max))])
-    (count-on
+    (count-layer/memo
      (for/list ([step steps]
                 #:when (x-within? x step))
        step))))
 
-(printf "Part-2: ~a\n" (part-2 (read-steps "day22-input.txt")))
+(define (part-1 steps)
+  (count-on
+   (for/list ([step steps])
+     (match-define (list s (cons x1 x2) (cons y1 y2) (cons z1 z2)) step)
+     (list s (cons (max -50 x1) (min 50 x2))
+           (cons (max -50 y1) (min 50 y2))
+           (cons (max -50 z1) (min 50 z2))))))
+
+(printf "Part-1: ~a\n" (part-1 (read-steps "day22-input.txt")))
+
+(printf "Part-2: ~a\n" (count-on (read-steps "day22-input.txt")))
 
 (module+ test
   (require rackunit)
@@ -92,4 +92,4 @@
   (check-equal? (vector-ceil #(0 10 20 30) 15) 2)
   (check-equal? (vector-ceil #(0 10 20 30) 31) 4)
   (check-equal? (part-1 (read-steps "day22-test-short.txt")) 590784)
-  (check-equal? (part-2 (read-steps "day22-test-long.txt")) 2758514936282235))
+  (check-equal? (count-on (read-steps "day22-test-long.txt")) 2758514936282235))
