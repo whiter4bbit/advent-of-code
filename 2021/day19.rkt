5d11a8af313fe7602ebc320e88fe8c81c1bbf1b2
Pavel Zalunin <wr4bbit@gmail.com>
2021-12-27 01:53:15 +0100

    Day 19: A little improvements

--- 2021/day19.rkt
+++ 2021/day19.rkt
@@ -22,17 +22,16 @@
 (define (coord- a b)
   (map - a b))
 
+(define (coord+ a b)
+  (map + a b))
+
 (define (find-second-offset/rotation sc-1 sc-2*)
   (for/or ([first-pos sc-1])
     (for/or ([(sc-2 i) (in-indexed sc-2*)])
       (for/or ([second-pos sc-2])
-        (define offsets-1
-          (for/fold ([offsets (set)])
-                    ([pos sc-1])
-            (set-add offsets (coord- pos first-pos))))
         (define intersection
           (for/sum ([pos sc-2]
-                    #:when (set-member? offsets-1 (coord- pos second-pos)))
+                    #:when (set-member? sc-1 (coord+ (coord- pos second-pos) first-pos)))
             1))
         (and (> intersection 11) (cons (coord- second-pos first-pos) i))))))
 
@@ -46,29 +45,31 @@
          [offset/rotation
           (match-define (cons offset rotation) offset/rotation)
           (define sc-1*
-            (for/fold ([total sc-1])
+            (for/fold ([sc-1 sc-1])
                       ([pos (list-ref sc* rotation)])
-              (set-add total (coord- pos offset))))
+              (set-add sc-1 (coord- pos offset))))
           (define scanners** (append (take scanners* i) (drop scanners* (add1 i))))
           (find-beacons/offsets sc-1* scanners** (cons offset offsets))]
          [else #f]))]))
 
 (define (part-1 scanners)
+  (define sc-1 (list->set (first scanners)))
   (define scanners* (map rotations* (rest scanners)))
-  (define beacons (car (find-beacons/offsets (list->set (first scanners)) scanners*)))
+  (define beacons (car (find-beacons/offsets sc-1 scanners*)))
   (set-count beacons))
 
 (define (part-2 scanners)
+  (define sc-1 (list->set (first scanners)))
   (define scanners* (map rotations* (rest scanners)))
-  (define offsets (cdr (find-beacons/offsets (list->set (first scanners)) scanners*)))
+  (define offsets (cdr (find-beacons/offsets sc-1 scanners*)))
   (for*/fold ([max-manh 0])
              ([a offsets]
               [b offsets])
     (define abs* (map (λ (a b) (abs (- a b))) a b))
     (max max-manh (apply + abs*))))
 
-; (printf "Part-1: ~a\n" (part-1 (read-scanners "day19-input.txt")))
-; (printf "Part-2: ~a\n" (part-2 (read-scanners "day19-input.txt")))
+(printf "Part-1: ~a\n" (part-1 (read-scanners "day19-input.txt")))
+(printf "Part-2: ~a\n" (part-2 (read-scanners "day19-input.txt")))
 
 (module+ test
   (require rackunit)
